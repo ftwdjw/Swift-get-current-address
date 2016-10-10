4d6deb94ab0d2347e013ebd6d920aa87c8fa8811
ftwdjw <ftwdjw@gmail.com>
2016-10-10 09:18:01 -0500

    delete unused variables

--- ViewController.swift
+++ ViewController.swift
@@ -21,8 +21,6 @@ class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelega
     
     @IBAction func getAddress(_ sender: AnyObject) {
          dropPin=true
-        localText = ""
-        print("\nlocal text=\(localText)\n")
         print("drop pin")
         self.locationManager.startUpdatingLocation()
     }
@@ -38,41 +36,13 @@ class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelega
     
  
     //variables
-    var localText = ""
     var measurement=0
-    var measurementSave = 0
-    var i=0
-    var latitude=0.0
-    var longitude=0.0
     var dropPin=true
-    var lastDropPin=false
-    let annotation = MKPointAnnotation()
-    let placemarks = MKPlacemark()//init for placemark data
-    var data : [String] = []
     let geocoder = CLGeocoder()
-    var myText = ""
     var locationManager = CLLocationManager()
+    let annotation = MKPointAnnotation()
     
-    
-    // MARK: - Properties
-    
-//    lazy var locationManager : CLLocationManager = {
-//        
-//        let locationManager = CLLocationManager()
-//        
-//        //Set up location manager with defaults
-//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-//        locationManager.distanceFilter = kCLDistanceFilterNone
-//        locationManager.delegate = self
-//        
-//        //Optimisation of battery
-//        locationManager.pausesLocationUpdatesAutomatically = true
-//        locationManager.activityType = CLActivityType.fitness
-//        locationManager.allowsBackgroundLocationUpdates = false
-//        
-//        return locationManager
-//    }()
-//
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -92,8 +62,6 @@ class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelega
         // Dispose of any resources that can be recreated.
     }
     
-    
-
 
     // MARK: - CLLocation didUpdateLocations
     
@@ -108,8 +76,8 @@ class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelega
         measurement += 1
         
         
-        latitude=location!.coordinate.latitude
-        longitude=location!.coordinate.longitude
+        let latitude=location!.coordinate.latitude
+        let longitude=location!.coordinate.longitude
         
         let center = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         
@@ -118,8 +86,6 @@ class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelega
         //print("center=\(center)")
         
         self.mapView.setRegion(region, animated: true)
-        
-        //self.geocoder.reverseGeocodeLocation(locationManager.location!, completionHandler:{ (placemarks,error) -> Void in } )
         
         self.geocoder.reverseGeocodeLocation(self.locationManager.location!, completionHandler: {(placemarks, error) -> Void in
             
@@ -194,106 +160,4 @@ class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelega
 
 
 }
-
-//    var localText : String = ""
-//    var postalCode : String = ""
-//    var title1 : [String] = [""]
-//    var subTitle1 : [String] = [""]
-//var location = CLLocation()
-//var center:CLLocationCoordinate2D
-
-// MARK: - getAddress
-
-//    func getAddress(){//start getAddress
-//        print("try to get address here")
-//
-//                    //start of reverseGeocoder
-//
-//                CLGeocoder().reverseGeocodeLocation(locationManager.location!, completionHandler: {(placemarks, error) -> Void in
-//                    print(self.locationManager.location)
-//
-//                    if error != nil {//start error
-//                        print("Reverse geocoder failed with error" + (error?.localizedDescription)!)
-//                        return
-//                    }//end error
-//
-//                        if (placemarks?.count)! > 0 {//start if
-//
-//                            print("\nprint out placemarks\n")
-//                            let pm = placemarks![0]
-//
-//
-//                            print("pm=\(pm)")
-//
-//                            print("\nthoroughfare=\(pm.thoroughfare!)\n")
-//                            print("\nsubThoroughfare=\(pm.subThoroughfare!)\n")
-//                             print("\n locality=\(pm.locality!)\n")
-//                            self.localText=pm.locality!
-//                             //print("\nsubLocality=\(pm.subLocality!)\n")
-//                            print("\nadministrativeArea=\(pm.administrativeArea!)\n")
-//                            print("\nsubAdministrativeArea=\(pm.subAdministrativeArea!)\n")
-//                            print("\npostalCode=\(pm.postalCode!)\n")
-//                            print("\ncountry=\(pm.country!)\n")
-//                            print("\nISOcountryCode=\(pm.isoCountryCode!)\n")
-//                            self.title1=[ (pm.subThoroughfare!) +  " " + (pm.thoroughfare!)]
-//                            self.subTitle1=[ (pm.locality!) +   " , " + (pm.administrativeArea!) + " " ]
-//                            self.subTitle1.append(pm.postalCode!)
-//                        }//end if
-//                        else {//start else
-//                            print("Problem with the data received from geocoder")
-//                        }//end else
-//                    })
-//                    //end of reverse geocoder
-//
-//
-//                    print("place=",localText)
-//                    annotation.title =  self.title1.joined(separator: " ")
-//                    annotation.subtitle=self.subTitle1.joined(separator: " ")
-//                    annotation.coordinate = (locationManager.location?.coordinate)!
-//                    mapView.addAnnotation(annotation)
-//                    measurementSave=measurement
-//                    print("\nadd annotation\n")
-//                    print("\nmeasurementSaved=\(measurementSave)\n")
-//
-//
-//                    let yourAnnotationAtIndex = 0
-//                    mapView.selectAnnotation(mapView.annotations[yourAnnotationAtIndex], animated: true)
-//
-//
-//                    self.locationManager.stopUpdatingLocation()
-//         }
-
-
-//            annotation.title = "Get My Location"
-//            annotation.subtitle = "           Address"
-//            annotation.coordinate = center
-//            mapView.addAnnotation(annotation)
-//            //select to show annotation
-//            mapView.selectAnnotation(annotation, animated: true)
-//            //let yourAnnotationAtIndex = 0
-//            //mapView.selectAnnotation(mapView.annotations[yourAnnotationAtIndex], animated: true)
-//            measurementSave=measurement
-//            print("\nadd annotation\n")
-//            print("\nmeasurementSaved=\(measurementSave)\n")
-
-
-
-//            print("place=",pm.locality!)
-//            annotation.title =  self.title1.joined(separator: " ")
-//            annotation.subtitle=self.subTitle1.joined(separator: " ")
-//            annotation.coordinate = (locationManager.location?.coordinate)!
-//            mapView.addAnnotation(annotation)
-//            measurementSave=measurement
-//            print("\nadd annotation\n")
-//            print("\nmeasurementSaved=\(measurementSave)\n")
-//
-//
-//            let yourAnnotationAtIndex = 0
-//            mapView.selectAnnotation(mapView.annotations[yourAnnotationAtIndex], animated: true)
-//
-//
-//            self.locationManager.stopUpdatingLocation()
-//
-//
-//
 
